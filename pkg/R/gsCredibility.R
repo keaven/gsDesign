1ddfbc55b89df3526dd2ee758307b4d2da779540
keaven <keaven@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-09-13 19:34:34 +0000

    Experimental routines not wanted in released package.

--- pkg/R/gsCredibility.R
+++
@@ -1,8 +0,0 @@
-gsCredibility<-function(x)
-{   pow<-x$upper$prob[,2]
-    for (i in 2:x$k) pow[i]<-pow[i]+pow[i-1]
-    if (x$test.type == 4 || x$test.type==6) alph<-x$falseposnb
-    else alph<-x$upper$prob[,1]
-    for (i in 2:x$k) alph[i]<-alph[i]+alph[i-1]
-    return(pow/alph)
-}
