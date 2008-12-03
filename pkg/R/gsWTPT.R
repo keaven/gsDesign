b5d2bb0eaea7639581a111ab5fdc8b2dc546f78d
pcombes <pcombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-03 23:21:22 +0000

    Merge from REvolution Computing
    Move .r files to .R and remove obsolete files

--- pkg/R/gsWTPT.R
+++ pkg/R/gsWTPT.R
@@ -1,26 +1,42 @@
-# Wang-Tsiatis boundary alpha comparison
-# for a timing vector, scalar a, and vector b, 
-# compute upper crossing probability using upper bound of c*b
-# a is used to control one- or 2-sided bound
-WTdiff<-function(c,alpha,a,b,timing,r)
-{	if (length(a)==1) a<- -c*b
-	x<-gsProbability(k=length(b),theta=0.,n.I=timing,a=a,b=c*b,r=r)
-	alpha-sum(x$upper$prob)
+"WTdiff" <- function(c, alpha, a, b, timing, r)
+{	
+    # Wang-Tsiatis boundary alpha comparison
+    # for a timing vector, scalar a, and vector b, 
+    # compute upper crossing probability using upper bound of c*b
+    # a is used to control one- or 2-sided bound
+    
+    if (length(a) == 1)
+    {
+        a <- -c * b
+    }
+	x <- gsprob(0., timing, a, c * b, r)
+	
+    alpha - sum(x$probhi)
 }
-# Wang-Tsiatis boundary
-# find constant for a Wang-Tsiatis bound to get appropriate alpha
-# for 2-sided, a=1 (set in gsDType2and5); otherwise, a set in gsDType1
-WT<-function(d,alpha,a,timing,tol=0.000001,r=18)
-{	b<-timing^(d-.5)
-	if (length(a)==1) c0<-0
-	else
-	{   i<-0
-	    while (WTdiff(i,alpha,a,b,timing,r)>=0.) i<-i-1
-	    c0<-i
-	}
-	i<-2
-	while(WTdiff(i,alpha,a,b,timing,r)<=0.) i<-i+1
-	c1<-i
-	return(uniroot(WTdiff,lower=c0,upper=c1,alpha=alpha,a=a,b=b,timing=timing,tol=tol,r=r)$root)
+
+"WT" <- function(d, alpha, a, timing, tol, r)
+{	
+    # Wang-Tsiatis boundary
+    # find constant for a Wang-Tsiatis bound to get appropriate alpha
+    # for 2-sided, a=1 (set in gsDType2and5); otherwise, a set in gsDType1
+    b <- timing^(d - 0.5)
+	i <- 0
+	
+    while (WTdiff(2^i, alpha, a, b, timing, r) >= 0.)
+    {
+        i <- i - 1
+    }
+	
+    c0 <- 2^i
+	i <- 2
+    
+	while (WTdiff(i, alpha, a, b, timing, r) <= 0.)
+    {
+        i <- i + 1
+    }
+    
+	c1 <- i
+	
+    uniroot(WTdiff, lower=c0, upper=c1, alpha=alpha, a=a, b=b, timing=timing, tol=tol, r=r)$root
 }
 
