6499b1f1fccb8f1b5f3c26b203dbea06478c12c4
b1uetrane <b1uetrane@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-09 19:33:15 +0000

    Stress tests

--- pkg/inst/unitTests/runit.gsDesign-stress.R
+++ pkg/inst/unitTests/runit.gsDesign-stress.R
@@ -1,44 +1,66 @@
+### Utility functions for gsDesign stress test functions
+
+"alpha.beta.range.util" <- function(alpha, beta, type, sf)
+{	
+    no.err <- TRUE
+	
+	for (a in alpha)
+	{
+	    for (b in beta)
+		{
+		    if (b < 1 - a)
+			{
+			    res <- try(gsDesign(test.type=type, alpha=a, beta=b, sfu=sf))
+			    
+			    if (is(res, "try-error"))
+			    {
+				    no.err <- FALSE
+			    }
+		    }
+		}		
+	}
+	
+	no.err
+}
+
+"param.range.util" <- function(param, type, sf)
+{	
+	no.err <- TRUE
+	
+	for (p in param)
+	{
+		res <- try(gsDesign(test.type=type, sfu=sf, sfupar=p))
+		
+		if (is(res, "try-error"))
+		{
+			no.err <- FALSE
+		}
+	}
+	
+	no.err
+}
+
+
 ### gsDesign stress test functions
 
-# NOT FINISHED
-#"stress.Pocock.type1" <- function()
-#{	
-#	no.errors <- TRUE
-#	
-#	for (a in seq(from=0.05, to=0.95, by=0.05))
-#	{
-#		
-#		for (b in seq(from=0.05, to=1-a-0.01, by=0.05))
-#		{
-#			res <- try(gsDesign(test.type=1, alpha=a, beta=b, sfu=sfLDPocock))
-#			
-#			if (is(res, "try-error"))
-#			{
-#				no.errors <- FALSE
-#			}
-#		}
-#		
-#	}
-#	
-#	checkTrue(no.errors, "Type 1 LDPocock stress test")
-#}
+"test.stress.sfLDPocock.type1" <- function()
+{	
+	a = seq(from=0.05, to=0.95, by=0.05)
+	b = seq(from=0.05, to=0.95, by=0.05)
+	
+    no.errors <- alpha.beta.range.util(alpha=a, beta=b, type=1, sf=sfLDPocock)
+	
+	checkTrue(no.errors, "Type 1 LDPocock stress test")
+}
 
 
 
 
 "test.stress.sfExp.type1" <- function()
 {	
-	no.errors <- TRUE
+	nu = seq(from=0.1, to=1.5, by=0.1)
 	
-	for (nu in seq(from=0.1, to=1.5, by=0.1))
-	{
-		res <- try(gsDesign(test.type=1, sfu=sfExponential, sfupar=nu))
-			
-		if (is(res, "try-error"))
-		{
-			no.errors <- FALSE
-		}
-	}
+	no.errors <- param.range.util(param=nu, type=1, sf=sfExponential)
 	
 	checkTrue(no.errors, "Type 1 sfExponential stress test")
 }
