e879831d5a08019cb376f2546048efa6ecb6404a
b1uetrane <b1uetrane@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-09 18:11:52 +0000

    New stress test

--- pkg/inst/unitTests/runit.gsDesign-stress.R
+++ pkg/inst/unitTests/runit.gsDesign-stress.R
@@ -1,5 +1,47 @@
 ### gsDesign stress test functions
 
+# NOT FINISHED
+#"stress.Pocock.type1" <- function()
+#{	
+#	no.errors <- TRUE
+#	
+#	for (a in seq(from=0.05, to=0.95, by=0.05))
+#	{
+#		
+#		for (b in seq(from=0.05, to=1-a-0.01, by=0.05))
+#		{
+#			res <- try(gsDesign(test.type=1, alpha=a, beta=b, sfu=sfLDPocock))
+#			
+#			if (is(res, "try-error"))
+#			{
+#				no.errors <- FALSE
+#			}
+#		}
+#		
+#	}
+#	
+#	checkTrue(no.errors, "Type 1 LDPocock stress test")
+#}
+
+
+
+
+"stress.sfExp.type1" <- function()
+{	
+	no.errors <- TRUE
+	
+	for (nu in seq(from=0.1, to=1.5, by=0.1))
+	{
+		res <- try(gsDesign(test.type=1, sfu=sfExponential, sfupar=nu))
+			
+		if (is(res, "try-error"))
+		{
+			no.errors <- FALSE
+		}
+	}
+	
+	checkTrue(no.errors, "Type 1 sfExponential stress test")
+}
 
 
 
