ee626e8c60b2fa67cbd6dd6ec684e80fa42e9153
wconstan <wconstan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-22 21:58:36 +0000

    GSDG-67
    * New R functionality to interface with Qt C++ calls. These functions coordinate also with Keaven's latest version of gsDesign, version 2.2-1.

--- pkg/gsDesignGUI/R/gsDesignGUI.R
+++ pkg/gsDesignGUI/R/gsDesignGUI.R
@@ -1,3 +1,32 @@
-gsDesignGUI <- function()
+"gsDesignGUI" <- function(x)
 {
+   # x is a named list of mapped Qt objects
+   z <- gsDesign(k=x$eptIntervalsSpin)
+
+   # form output to send back to Qt
+   gsDesignToQt(z, x)
 }
+
+
+"gsDesignToQt" <- function(x, designList, plotPath=file.path(tempdir(), "gsDesignPlot.png"))
+{
+  if (!is(x, "gsDesign"))
+  {
+    stop("x must be an object of class \"gDesign\"")
+  }
+  
+  # capture textual output
+  outText <- paste(capture.output(print(x)), collapse="\n")
+  
+  # capture graphical output
+  png(file=plotPath, bg="transparent")
+  types <- c("Boundaries", "Power", "Treatment Effect", "Conditional Power", "Spending Function","Expected Sample Size","B-Values")
+  plot(x, plottype=match(designList$opTypeCombo, types))
+  dev.off()
+  
+  list(text=outText, plot=plotPath)
+}
+  
+    
+
+
