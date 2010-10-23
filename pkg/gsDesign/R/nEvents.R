d26cab3af0994cbc6655e3c58c6cb6784a541dde
keaven <keaven@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-23 21:59:06 +0000

    nEvents: corrected se calc and reorganized table output option

--- pkg/gsDesign/R/nEvents.R
+++ pkg/gsDesign/R/nEvents.R
@@ -3,14 +3,16 @@
     delta <- -c * (log(hr) - log(hr0))
     if (n[1] == 0)
     {   n <- (qnorm(1-alpha)+qnorm(1-beta))^2/delta^2
-        if (tbl) n <- cbind(hr, n = ceiling(n), alpha=alpha, beta=beta, Power=1-beta, delta=delta, 
-                            ratio=ratio, se=sqrt(1/c/ceiling(n)))
+        if (tbl) n <- cbind(hr = hr, n = ceiling(n), alpha = alpha, beta = beta, 
+                            Power = 1-beta, delta = delta, ratio = ratio, 
+                            hr0 = hr0, se = 1/c/sqrt(ceiling(n)))
         return(n)
     }
     else
     {   pwr <- pnorm(-(qnorm(1-alpha)-sqrt(n) * delta))
-        if (tbl) pwr <- cbind(hr, n=n, alpha=alpha, beta=1-pwr, Power=pwr, delta=delta,
-                 ratio=ratio, se=sqrt(1/(c*n)))
+        if (tbl) pwr <- cbind(hr = hr, n = n, alpha = alpha, beta = 1-pwr,
+                            Power = pwr, delta = delta, ratio = ratio,
+                            hr0 = hr0, se = sqrt(1/n)/c)
         return(pwr)
     }
 }
