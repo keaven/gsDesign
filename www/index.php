564d74138fb3a95f29cd2a650f3fe99880e1db22
"tony" <"tony"@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-26 01:02:03 +0000

    Updating project web page to add link for gsDesign build instructions.

--- www/index.php
+++ www/index.php
@@ -43,9 +43,9 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
-
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+
+<p><a href="build-instructions.html">Build Instructions</a></p>
 
 </body>
 </html>
