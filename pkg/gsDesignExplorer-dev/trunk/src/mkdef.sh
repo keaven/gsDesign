2918fddd81978dcb8bff59fc631622ba7045cd44
tony <tony"@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-08 22:10:08 +0000

    Adding current gsDesignExplorer to gsDesignExplorer-dev/trunk

---
+++ pkg/gsDesignExplorer-dev/trunk/src/mkdef.sh
@@ -0,0 +1,6 @@
+echo LIBRARY libgsDesignExplorer.dll > gsDesignExplorer.def
+echo EXPORTS >> gsDesignExplorer.def
+nm -g --defined-only CMakeFiles/gsDesignExplorer.dir/*.obj > tmp
+sed -n '/^........ [T|C|B] _/s/^........ [T|C|B] _/ /p' tmp >> gsDesignExplorer.def
+rm tmp
+
