3f335a1a84a3ec3e6215e70231f894382b0c7c54
"tony" <"tony"@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-08 22:10:46 +0000

    Deleting obsolete gsDesignGUI branch

--- pkg/gsDesignGUI/src/explorergui/mkdef.sh
+++
@@ -1,5 +0,0 @@
-echo EXPORTS > gsdesigngui.def
-nm -g --defined-only CMakeFiles/gsdesigngui.dir/*.obj > tmp
-sed -n '/^........ [T|C|B] _/s/^........ [T|C|B] _/ /p' tmp >> gsdesigngui.def
-rm tmp
-
--- pkg/gsDesignGUI/src/mkdef.sh
+++
@@ -1,5 +0,0 @@
-echo EXPORTS > gsDesignExplorer.def
-nm -g --defined-only CMakeFiles/gsDesignExplorer.dir/*.obj > tmp
-sed -n '/^........ [T|C|B] _/s/^........ [T|C|B] _/ /p' tmp >> gsDesignExplorer.def
-rm tmp
-
