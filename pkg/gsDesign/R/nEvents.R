51176852f481ad8a3366043b3a4de302c616fa1c
keaven <keaven@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-12 22:31:36 +0000

    Fixed sided=2 for nEvents

--- pkg/gsDesign/R/nEvents.R
+++ pkg/gsDesign/R/nEvents.R
@@ -1,16 +1,17 @@
 "nEvents" <- function(hr = .6, alpha = .025, beta = .1, ratio = 1, sided = 1, hr0 =  1, n = 0, tbl = FALSE)
-{   c <- sqrt(ratio) / (1 + ratio)
+{   if (sided != 1 && sided != 2) stop("sided must be 1 or 2")
+    c <- sqrt(ratio) / (1 + ratio)
     delta <- -c * (log(hr) - log(hr0))
     if (n[1] == 0)
-    {   n <- (qnorm(1-alpha)+qnorm(1-beta))^2/delta^2
-        if (tbl) n <- cbind(hr = hr, n = ceiling(n), alpha = alpha, beta = beta, 
+    {   n <- (qnorm(1-alpha/sided)+qnorm(1-beta))^2/delta^2
+        if (tbl) n <- cbind(hr = hr, n = ceiling(n), alpha = alpha, sided=sided, beta = beta, 
                             Power = 1-beta, delta = delta, ratio = ratio, 
                             hr0 = hr0, se = 1/c/sqrt(ceiling(n)))
         return(n)
     }
     else
-    {   pwr <- pnorm(-(qnorm(1-alpha)-sqrt(n) * delta))
-        if (tbl) pwr <- cbind(hr = hr, n = n, alpha = alpha, beta = 1-pwr,
+    {   pwr <- pnorm(-(qnorm(1-alpha/sided)-sqrt(n) * delta))
+        if (tbl) pwr <- cbind(hr = hr, n = n, alpha = alpha, sided=sided, beta = 1-pwr,
                             Power = pwr, delta = delta, ratio = ratio,
                             hr0 = hr0, se = sqrt(1/n)/c)
         return(pwr)
