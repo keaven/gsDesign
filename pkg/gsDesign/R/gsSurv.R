d2976224cc4f7e8db6efb0e822ada8a74a322e10
keaven <keaven@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-06-01 14:19:11 +0000

    Additional minor 2.8 updates, including vignettes for time-to-event endpoints.

--- pkg/gsDesign/R/gsSurv.R
+++ pkg/gsDesign/R/gsSurv.R
@@ -4,55 +4,57 @@
 eEvents1<-function(lambda=1, eta=0, gamma=1, R=1, S=NULL, 
                    T=2, Tfinal=NULL, minfup=0, simple=TRUE)
 {  
-   if (is.null(Tfinal))
-   {   Tfinal <- T
-       minfupia <- minfup
-   }
-   else minfupia <- max(0, minfup-(Tfinal - T))
-
-   nlambda <- length(lambda)
-   if (length(eta)==1 & nlambda > 1) 
-     eta <- array(eta,nlambda)
-   T1 <- cumsum(S)
-   T1 <- c(T1[T1<T],T)
-   T2 <- T - cumsum(R)
-   T2[T2 < minfupia] <- minfupia
-   i <- 1:length(gamma)
-   gamma[i>length(unique(T2))] <- 0
-   T2 <- unique(c(T,T2[T2 > 0]))
-   T3 <- sort(unique(c(T1,T2)))
-   if (sum(R) >= T) T2 <- c(T2,0)
-   nperiod <- length(T3)
-   s <- T3-c(0,T3[1:(nperiod-1)])
-
-   lam <- array(lambda[nlambda],nperiod)
-   et <- array(eta[nlambda],nperiod)
-   gam <- array(0,nperiod)
-
-   for(i in length(T1):1)
-   {  indx <- T3<=T1[i]
-      lam[indx]<-lambda[i]
-      et[indx]<-eta[i]
-   }
-   for(i in min(length(gamma)+1,length(T2)):2) 
-     gam[T3>T2[i]]<-gamma[i-1]
-   q <- exp(-(lam+et)*s)
-   Q <- cumprod(q)
-   indx<-1:(nperiod-1)
-   Qm1 <- c(1,Q[indx])
-   p <- lam/(lam+et)*Qm1*(1-q)
-   P <- cumsum(p)
-   B <- gam/(lam+et)*lam*(s-(1-q)/(lam+et))
-   A <- c(0,P[indx])*gam*s+Qm1*B
-   if (!simple)
-      return(list(lambda=lambda, eta=eta, gamma=gamma, R=R, S=S,
-                  T=T, Tfinal=Tfinal, minfup=minfup, d=sum(A), 
-                  n=sum(gam*s), q=q,Q=Q,p=p,P=P,B=B,A=A,T1=T1,
-                  T2=T2,T3=T3,lam=lam,et=et,gam=gam))
-   else
-      return(list(lambda=lambda, eta=eta, gamma=gamma, R=R, S=S,  
-                  T=T, Tfinal=Tfinal, minfup=minfup, d=sum(A), 
-                  n=sum(gam*s)))
+  if (is.null(Tfinal))
+  {   Tfinal <- T
+      minfupia <- minfup
+  }
+  else minfupia <- max(0, minfup-(Tfinal - T))
+  
+  nlambda <- length(lambda)
+  if (length(eta)==1 & nlambda > 1) 
+    eta <- array(eta,nlambda)
+  T1 <- cumsum(S)
+  T1 <- c(T1[T1<T],T)
+  T2 <- T - cumsum(R)
+  T2[T2 < minfupia] <- minfupia
+  i <- 1:length(gamma)
+  gamma[i>length(unique(T2))] <- 0
+  T2 <- unique(c(T,T2[T2 > 0]))
+  T3 <- sort(unique(c(T1,T2)))
+  if (sum(R) >= T) T2 <- c(T2,0)
+  nperiod <- length(T3)
+  s <- T3-c(0,T3[1:(nperiod-1)])
+  
+  lam <- array(lambda[nlambda],nperiod)
+  et <- array(eta[nlambda],nperiod)
+  gam <- array(0,nperiod)
+  
+  for(i in length(T1):1)
+  {  indx <- T3<=T1[i]
+     lam[indx]<-lambda[i]
+     et[indx]<-eta[i]
+  }
+  for(i in min(length(gamma)+1,length(T2)):2) 
+    gam[T3>T2[i]]<-gamma[i-1]
+  q <- exp(-(lam+et)*s)
+  Q <- cumprod(q)
+  indx<-1:(nperiod-1)
+  Qm1 <- c(1,Q[indx])
+  p <- lam/(lam+et)*Qm1*(1-q)
+  p[is.nan(p)] <- 0 
+  P <- cumsum(p)
+  B <- gam/(lam+et)*lam*(s-(1-q)/(lam+et))
+  B[is.nan(B)]<-0
+  A <- c(0,P[indx])*gam*s+Qm1*B
+  if (!simple)
+    return(list(lambda=lambda, eta=eta, gamma=gamma, R=R, S=S,
+                T=T, Tfinal=Tfinal, minfup=minfup, d=sum(A), 
+                n=sum(gam*s), q=q,Q=Q,p=p,P=P,B=B,A=A,T1=T1,
+                T2=T2,T3=T3,lam=lam,et=et,gam=gam))
+  else
+    return(list(lambda=lambda, eta=eta, gamma=gamma, R=R, S=S,  
+                T=T, Tfinal=Tfinal, minfup=minfup, d=sum(A), 
+                n=sum(gam*s)))
 }
 ###################################################
 ### code chunk number 4: eEvents
