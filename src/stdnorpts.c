329ae2a45c591bfe8fb1193c60a7b3f547486a9f
Harsh Baid <harshb@metrumrg.com>
2019-03-05 14:59:55 -0500

    refactor: adding headers/creating c files

--- src/stdnorpts.c
+++ src/stdnorpts.c
@@ -1,3 +1,5 @@
+#include "gsDesign.h"
+
 void stdnorpts(int *r,double *bounds,double *z,double *w)
 {  int gridpts(int,double,double,double,double *,double *);
 	gridpts(r[0],0.,bounds[0],bounds[1],z,w);
