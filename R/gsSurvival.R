eb34bc783446ec22d14fee38ccfc8d510bfb9f82
Keaven Anderson <temzq@yahoo.com>
2020-03-26 14:37:14 -0400

    Minor help file formatting correction

--- R/gsSurvival.R
+++ R/gsSurvival.R
@@ -48,8 +48,8 @@
 #' hazard ratio into the above equation allows approximating any one of the
 #' following based on the other two: the estimate hazard ratio, the number of
 #' events and the z-statistic. That is, \deqn{\hat{\lambda}=
-#' \exp(z(1+r)/\sqrt{rn})} \deqn{z=log(\hat{\lambda})\sqrt{nr}/(1+r)} \deqn{n=
-#' (z(1+r)/log(\hat{\lambda}))^2/r.}
+#' \exp(z(1+r)/\sqrt{rn})} \deqn{z=\log(\hat{\lambda})\sqrt{nr}/(1+r)} \deqn{n=
+#' (z(1+r)/\log(\hat{\lambda}))^2/r.}
 #'
 #' \code{hrz2n()} translates an observed interim hazard ratio and interim
 #' z-value into the number of events required for the Z-value and hazard ratio
