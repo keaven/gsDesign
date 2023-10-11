3305b2fe698ca10a1e56d757bbb50cd056bf1948
Bingjun(Benjamin) Wang <85646030+wangben718@users.noreply.github.com>
2023-10-11 09:55:24 -0400

    Add `as_rtf()` for `gsBinomialExactTable()` (#102)
    * add as_rtf for gsBinomialExactTable
    
    * add r2rtf dependency
    
    * Yujie's review: move and add examples
    
    * fix cmd check
    
    * minor change for default value
    
    * add documentation for new argument
    
    * rerun documentation
    
    * remove subtitle
    
    * address coding style issue
    
    * change documentation
    
    * return invisible input
    
    * style the code
    
    * use label instead of number
    
    * capitalize lower
    
    * return original x
    
    * pass S3 check
    
    * remove "..."
    
    * add ... to method as_rtf
    
    * rewrite documentation
    
    * add back ...
    
    * Put `...` after required arguments; improve code and documentation style
    
    * change default value of bound_label
    
    * Style code
    
    ---------
    
    Co-authored-by: Zhao, Yujie <yujie.zhao@merck.com>
    Co-authored-by: Nan Xiao <nan.xiao1@merck.com>

--- R/as_gt.R
+++ R/as_gt.R
@@ -1,13 +1,13 @@
-#' Print a summary table using gt
+#' Convert a summary table object to a gt object
 #'
-#' Create print a table created with \code{\link{as_table}} to summarize
-#' an object and print it using \code{\link[gt]{gt}}; currently only
-#' implemented for \code{\link{gsBinomialExact}}.
+#' Convert a summary table object created with \code{\link{as_table}}
+#' to a \code{gt_tbl} object; currently only implemented for
+#' \code{\link{gsBinomialExact}}.
 #'
-#' @param x   Object to be printed using \code{\link[gt]{gt}}.
-#' @param ... Other parameters that may be specific the object.
+#' @param x Object to be converted.
+#' @param ... Other parameters that may be specific to the object.
 #'
-#' @return A `gt` object that may be extended by overloaded versions of
+#' @return A \code{gt_tbl} object that may be extended by overloaded versions of
 #'   \code{\link{as_gt}}.
 #'
 #' @seealso \code{vignette("binomialSPRTExample")}
@@ -21,7 +21,9 @@
 #' @export
 #'
 #' @examples
-#' safety_design <- binomialSPRT(p0 = .04, p1 = .1, alpha = .04, beta = .2, minn = 4, maxn = 75)
+#' safety_design <- binomialSPRT(
+#'   p0 = .04, p1 = .1, alpha = .04, beta = .2, minn = 4, maxn = 75
+#' )
 #' safety_power <- gsBinomialExact(
 #'   k = length(safety_design$n.I),
 #'   theta = seq(.02, .16, .02),
@@ -52,28 +54,29 @@ as_gt <- function(x, ...) UseMethod("as_gt")
 #' @importFrom gt gt tab_spanner cols_label html fmt_number fmt_percent tab_header
 #'
 #' @export
-as_gt.gsBinomialExactTable <-
-  function(x,
-           title = "Operating Characteristics for the Truncated SPRT Design",
-           subtitle = "Assumes trial evaluated sequentially after each response",
-           theta_label = html("Underlying<br>response rate"),
-           bound_label = c("Futility bound", "Efficacy bound"),
-           prob_decimals = 2,
-           en_decimals = 1,
-           rr_decimals = 0,
-           ...) {
-    out_gt <- x %>%
-      gt() %>%
-      tab_spanner(label = "Probability of crossing", columns = c(Lower, Upper)) %>%
-      cols_label(
-        theta = theta_label,
-        Lower = bound_label[1],
-        Upper = bound_label[2],
-        en = html("Average<br>sample size")
-      ) %>%
-      fmt_number(columns = c(Lower, Upper), decimals = prob_decimals) %>%
-      fmt_number(columns = en, decimals = en_decimals) %>%
-      fmt_percent(columns = theta, decimals = rr_decimals) %>%
-      tab_header(title = title, subtitle = subtitle)
-    return(out_gt)
-  }
+as_gt.gsBinomialExactTable <- function(
+    x,
+    ...,
+    title = "Operating Characteristics for the Truncated SPRT Design",
+    subtitle = "Assumes trial evaluated sequentially after each response",
+    theta_label = html("Underlying<br>response rate"),
+    bound_label = c("Futility bound", "Efficacy bound"),
+    prob_decimals = 2,
+    en_decimals = 1,
+    rr_decimals = 0) {
+  out_gt <- x %>%
+    gt() %>%
+    tab_spanner(label = "Probability of crossing", columns = c(Lower, Upper)) %>%
+    cols_label(
+      theta = theta_label,
+      Lower = bound_label[1],
+      Upper = bound_label[2],
+      en = html("Average<br>sample size")
+    ) %>%
+    fmt_number(columns = c(Lower, Upper), decimals = prob_decimals) %>%
+    fmt_number(columns = en, decimals = en_decimals) %>%
+    fmt_percent(columns = theta, decimals = rr_decimals) %>%
+    tab_header(title = title, subtitle = subtitle)
+
+  out_gt
+}
---
+++ R/as_rtf.R
@@ -0,0 +1,114 @@
+#' Save a summary table object as an RTF file
+#'
+#' Convert and save the summary table object created with \code{\link{as_table}}
+#' to an RTF file using r2rtf; currently only implemented for
+#' \code{\link{gsBinomialExact}}.
+#'
+#' @param x Object to be saved as RTF file.
+#' @param ... Other parameters that may be specific to the object.
+#'
+#' @return \code{as_rtf()} returns the input \code{x} invisibly.
+#'
+#' @seealso \code{vignette("binomialSPRTExample")}
+#'
+#' @details
+#' Currently only implemented for \code{\link{gsBinomialExact}} objects.
+#' Creates a table to summarize an object.
+#' For \code{\link{gsBinomialExact}}, this summarized operating characteristics
+#' across a range of effect sizes.
+#'
+#' @export
+#'
+#' @examples
+#' zz <- gsBinomialExact(
+#'   k = 3, theta = seq(0, 1, 0.1), n.I = c(12, 24, 36),
+#'   a = c(-1, 0, 11), b = c(5, 9, 12)
+#' )
+#' zz %>%
+#'   as_table() %>%
+#'   as_rtf(
+#'     file = tempfile(fileext = ".rtf"),
+#'     title = "Power/Type I Error and Expected Sample Size for a Group Sequential Design"
+#'   )
+#'
+#' safety_design <- binomialSPRT(
+#'   p0 = .04, p1 = .1, alpha = .04, beta = .2, minn = 4, maxn = 75
+#' )
+#' safety_power <- gsBinomialExact(
+#'   k = length(safety_design$n.I),
+#'   theta = seq(.02, .16, .02),
+#'   n.I = safety_design$n.I,
+#'   a = safety_design$lower$bound,
+#'   b = safety_design$upper$bound
+#' )
+#' safety_power %>%
+#'   as_table() %>%
+#'   as_rtf(
+#'     file = tempfile(fileext = ".rtf"),
+#'     theta_label = "Underlying\nAE Rate",
+#'     prob_decimals = 3,
+#'     bound_label = c("Low Rate", "High Rate")
+#'   )
+as_rtf <- function(x, ...) UseMethod("as_rtf")
+
+#' @rdname as_rtf
+#'
+#' @param file File path for the output.
+#' @param title Table title.
+#' @param theta_label Label for theta.
+#' @param response_outcome Logical values indicating if the outcome is
+#'   response rate (\code{TRUE}) or failure rate (\code{FALSE}).
+#'   The default value is \code{TRUE}.
+#' @param bound_label Label for bounds.
+#'   If the outcome is response rate, then the label is "Futility bound"
+#'   and "Efficacy bound".
+#'   If the outcome is failure rate, then the label is "Efficacy bound"
+#'   and "Futility bound".
+#' @param en_label Label for expected number.
+#' @param prob_decimals Number of decimal places for probability of crossing.
+#' @param en_decimals Number of decimal places for expected number of
+#'   observations when bound is crossed or when trial ends without crossing.
+#' @param rr_decimals Number of decimal places for response rates.
+#'
+#' @importFrom r2rtf rtf_title rtf_colheader rtf_body rtf_encode write_rtf
+#'
+#' @export
+as_rtf.gsBinomialExactTable <- function(
+    x,
+    file,
+    ...,
+    title = paste(
+      "Operating Characteristics by Underlying Response Rate for",
+      "Exact Binomial Group Sequential Design"
+    ),
+    theta_label = "Underlying Response Rate",
+    response_outcome = TRUE,
+    bound_label = if (response_outcome) c("Futility Bound", "Efficacy Bound") else c("Efficacy Bound", "Futility Bound"),
+    en_label = "Expected Sample Sizes",
+    prob_decimals = 2,
+    en_decimals = 1,
+    rr_decimals = 0) {
+  x_out <- x
+
+  x[, "Lower"] <- sprintf(paste0("%.", prob_decimals, "f"), unlist(x[, "Lower"]))
+  x[, "Upper"] <- sprintf(paste0("%.", prob_decimals, "f"), unlist(x[, "Upper"]))
+  x[, "theta"] <- paste0(sprintf(paste0("%.", rr_decimals, "f"), unlist(x[, "theta"] * 100)), "%")
+  x[, "en"] <- sprintf(paste0("%.", en_decimals, "f"), unlist(x[, "en"]))
+
+  x %>%
+    rtf_title(title = title) %>%
+    rtf_colheader(
+      paste0(theta_label, " | ", "Probability of Crossing", " | ", en_label),
+      col_rel_width = c(1, 2, 1)
+    ) %>%
+    rtf_colheader(paste0(" | ", bound_label[1], " | ", bound_label[2], " | "),
+      col_rel_width = c(1, 1, 1, 1),
+      border_top = c("", rep("single", 2), ""),
+      border_left = c("single", "single", "", "single")
+    ) %>%
+    rtf_body() %>%
+    rtf_encode() %>%
+    write_rtf(file)
+
+  invisible(x_out)
+}
