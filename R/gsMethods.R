b8ff1ab3e2c8ba9e82df66b92d71f0296458db12
keaven <keaven_anderson@merck.com>
2023-10-24 06:10:40 -0400

    fixed rounding issue when event counts are integers

--- R/gsMethods.R
+++ R/gsMethods.R
@@ -690,7 +690,7 @@ gsBoundSummary <- function(x, deltaname = NULL, logdelta = FALSE, Nname = NULL, 
     nstat <- 2
   } else {
     nstat <- 4
-    statframe[statframe$Value == statframe$Value[3], ]$Analysis <- paste("Events:", ceiling(rowSums(x$eDC + x$eDE)))
+    statframe[statframe$Value == statframe$Value[3], ]$Analysis <- paste("Events:", ceiling(x$n.I))
     if (x$ratio == 1) N <- 2 * ceiling(rowSums(x$eNE)) else N <- ceiling(rowSums(x$eNE)) + ceiling(rowSums(x$eNC))
     Time <- round(x$T, tdigits)
     statframe[statframe$Value == statframe$Value[4], ]$Analysis <- paste(timename, ": ", as.character(Time), sep = "")
