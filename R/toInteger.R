e774a1af047dc5a69d7fb86d84fc4bd09e86aecf
Nan Xiao <nan.xiao1@merck.com>
2024-11-08 13:18:40 -0500

    Format toInteger roxygen docs and add vignette link

--- R/toInteger.R
+++ R/toInteger.R
@@ -2,26 +2,37 @@
 #' or sample size (other designs)
 #'
 #' @param x An object of class \code{gsDesign} or \code{gsSurv}.
-#' @param ratio A non-negative integer, usually corresponding to experimental:control sample size ratio. 
-#' A non-negative integer is required.
-#' Rounding is done to a multiple of \code{ratio + 1}. 
-#' See details.
-#' @param roundUpFinal Sample size is rounded up to a value of \code{ratio + 1} with the default \code{roundUpFinal = TRUE}.
-#' If \code{roundUpFinal = FALSE}, sample size is rounded to the nearest multiple of \code{ratio + 1}.
-#' For event counts, \code{roundUpFinal=TRUE} rounds final event count up; otherwise, just rounded if \code{roundUpFinal = FALSE}.
-#' See details. 
+#' @param ratio A non-negative integer, usually corresponding to
+#'   experimental:control sample size ratio.
+#'   A non-negative integer is required.
+#'   Rounding is done to a multiple of \code{ratio + 1}.
+#'   See details.
+#' @param roundUpFinal Sample size is rounded up to a value of \code{ratio + 1}
+#'   with the default \code{roundUpFinal = TRUE}.
+#'   If \code{roundUpFinal = FALSE}, sample size is rounded to the nearest
+#'   multiple of \code{ratio + 1}.
+#'   For event counts, \code{roundUpFinal = TRUE} rounds final event count up;
+#'   otherwise, just rounded if \code{roundUpFinal = FALSE}.
+#'   See details.
 #'
-#' @return Output is an object of the same class as input \code{x}; i.e., \code{gsDesign} with integer vector for \code{n.I}
-#' or \code{gsSurv} with integer vector \code{n.I} and integer total sample size. See details.
-#' 
+#' @return Output is an object of the same class as input \code{x}; i.e.,
+#'   \code{gsDesign} with integer vector for \code{n.I} or \code{gsSurv}
+#'   with integer vector \code{n.I} and integer total sample size. See details.
+#'
 #' @details
-#' The default of \code{ratio = 0, roundUpFinal = TRUE} will just round up the sample size (also event count) being rounded up.
+#' The default of \code{ratio = 0, roundUpFinal = TRUE} will just round up
+#' the sample size (also event count) being rounded up.
 #' Rounding of event count targets is not impacted by \code{ratio}.
-#' A positive integer value of \code{ratio} is required and the sample size will be rounded to a multiple of \code{ratio + 1}.
-#' The most common example would be if there is 1:1 randomization and the user wishes an even sample size, then set \code{ratio = 1}.
-#' If \code{ratio = 3}, rounding for final sample size is done to a multiple of 3 + 1 = 4; 
-#' this could represent a 3:1 or 1:3 randomization ratio.
-#' For 3:2 randomization, \code{ratio = 4} would ensure rounding sample size to a multiple of 5.
+#' A positive integer value of \code{ratio} is required and the sample size
+#' will be rounded to a multiple of \code{ratio + 1}.
+#' The most common example would be if there is 1:1 randomization and the user
+#' wishes an even sample size, then set \code{ratio = 1}.
+#' If \code{ratio = 3}, rounding for final sample size is done to a multiple of
+#' 3 + 1 = 4;  this could represent a 3:1 or 1:3 randomization ratio.
+#' For 3:2 randomization, \code{ratio = 4} would ensure rounding sample size
+#' to a multiple of 5.
+#'
+#' See \code{vignette("toInteger")} for more examples with further details.
 #'
 #' @export
 #'
@@ -88,7 +99,7 @@ toInteger <- function(x, ratio = 0, roundUpFinal = TRUE) {
   if (inherits(x, "gsSurv") || x$nFixSurv > 0) {
     xi$hr0 <- x$hr0 # H0 hazard ratio
     xi$hr <- x$hr # H1 hazard ratio
-    
+
     N <- rowSums(x$eNC + x$eNE)[x$k] # get input total sample size
     N_continuous <- N
     # Update sample size to integer
