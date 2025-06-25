5102de52246a2189974fbf8e73d1d18a6218e835
Nan Xiao <me@nanx.me>
2025-06-25 01:24:26 -0400

    Add dplyr variable from `binomialPowerTable()`

--- R/globals.R
+++ R/globals.R
@@ -2,7 +2,9 @@ utils::globalVariables(
   unique(
     c(
       # From `as_gt.gsBinomialExactTable()`
-      c("Lower", "Upper", "en")
+      c("Lower", "Upper", "en"),
+      # From `binomialPowerTable()`
+      c("pE")
     )
   )
 )
