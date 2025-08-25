9f3fba3c1fe81773c5fd904b4af9794ca15a6988
Nan Xiao <me@nanx.me>
2025-08-25 11:17:00 -0400

    Add missing imports and symbols to avoid `R CMD check` notes

--- R/globals.R
+++ R/globals.R
@@ -2,7 +2,7 @@ utils::globalVariables(
   unique(
     c(
       # From `as_gt.gsBinomialExactTable()`
-      c("Lower", "Upper", "en"),
+      c("Lower", "Upper", "en", "theta"),
       # From `binomialPowerTable()`
       c("pE")
     )
