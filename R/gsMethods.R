be43bc7b25e34e237acafce4c343773a3cf496e0
Nan Xiao <me@nanx.me>
2025-12-06 00:53:39 -0500

    Tighten calendar-time detection to gsNB in gsBoundSummary

--- R/gsMethods.R
+++ R/gsMethods.R
@@ -426,7 +426,7 @@ gsBoundSummary0 <- function(
     if (x$n.fix > 1) N <- ceiling(x$n.I) else N <- round(x$n.I, 2)
     if (Nname == "Information") N <- round(x$n.I, 2)
     # Check if calendar time T is provided (for non-gsSurv objects like gsNB)
-    if (!is.null(x$T)) {
+    if (inherits(x, "gsNB") && !is.null(x$T)) {
       nstat <- 3
       Time <- round(x$T, tdigits)
       statframe[statframe$Value == statframe$Value[3], ]$Analysis <- paste(timename, ": ", as.character(Time), sep = "")
